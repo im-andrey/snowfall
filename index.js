26983bb1acb70a0ca53b18b7190552bb0df92780
Andrey <58787957+im-andrey@users.noreply.github.com>
2023-11-24 00:18:25 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const hoursEl = document.getElementById('hours')
 const minsEl = document.getElementById('mins')
 const secondsEl = document.getElementById('seconds')
 
-const newYears = '01 Jan 2022';
+const newYears = '01 Jan 2024';
 let toggle = true
 var timer
 
